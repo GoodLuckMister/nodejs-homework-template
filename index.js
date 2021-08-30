72f797e8e4739d39c1a0032a3b419e253f7aa066
GodLuckMister <paet14@gmail.com>
2021-08-30 23:50:58 +0300

    update

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const createFolderIsNotExist = require('./src/helpers/create-folder')
 const path = require('path')
 require('dotenv').config()
 
-
-
 const PORT = process.env.PORT || 3000
 const UPLOAD_DIR = process.env.UPLOAD_DIR
 const STATIC_OF_USERS = process.env.STATIC_OF_USERS
