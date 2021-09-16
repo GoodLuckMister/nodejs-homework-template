ec1841c227eb35abd7bf88e4bebed06f4df9e401
GodLuckMister <paet14@gmail.com>
2021-09-17 00:15:58 +0300

    add upload folder

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const app = require('./src/app')
 
 const client = require('./src/db')
-// const createFolderIsNotExist = require('./src/helpers/create-folder')
+const createFolderIsNotExist = require('./src/helpers/create-folder')
 // const path = require('path')
 require('dotenv').config()
 
 
 const PORT = process.env.PORT || 3000
-// const UPLOAD_DIR = process.env.UPLOAD_DIR
+const UPLOAD_DIR = process.env.UPLOAD_DIR
 
 // const STATIC_OF_USERS = process.env.STATIC_OF_USERS
 // const AVATARS = process.env.AVATARS
 
 client.then(() => {
-    app.listen(PORT, () => {
-        // await createFolderIsNotExist(UPLOAD_DIR)
+    app.listen(PORT, async () => {
+        await createFolderIsNotExist(UPLOAD_DIR)
         // await createFolderIsNotExist(STATIC_OF_USERS)
         // await createFolderIsNotExist(path.join(STATIC_OF_USERS, AVATARS))
         console.log(`Server running. Use our API on port: ${PORT}`)
